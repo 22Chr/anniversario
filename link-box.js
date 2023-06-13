4268e4decd15ee705169ea4e4e657ee69f8717d6
22Chr <136465607+22Chr@users.noreply.github.com>
2023-06-13 16:02:12 +0200

    v1

---
+++ link-box.js
@@ -0,0 +1,11 @@
+function openNav() {
+    if (x.matches) {
+    document.getElementById("myNav").style.width = "100%";
+    } else {
+    document.getElementById("myNav").style.width = "25%";
+    }
+}
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+var x = window.matchMedia("(max-width: 500px)")    
